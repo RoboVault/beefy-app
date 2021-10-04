c2ea32f2a64e34eaa086542e298dc4956fdaf4db
SmoothBot <smoothbot123@gmail.com>
2021-10-04 15:11:31 +1100

    Adding v1 link banner

--- src/components/Disclaimer/Disclaimer.js
+++ src/components/Disclaimer/Disclaimer.js
@@ -13,15 +13,25 @@ const Disclaimer = () => {
   const classes = useStyles();
 
   return (
-    <Grid container item className={classes.root} justify="center">
-      {
-        <Typography className={classes.disclaimer}>{t('Disclaimer')} 
-            <a href="https://docs.robo-vault.com/robovault/risks" rel="noreferrer">
-              <i className={`fas fa-book ${classes.linkIcon}`}></i>
-            </a>
-        </Typography>
-      }
-    </Grid>
+    <>
+      <Grid container item className={classes.root} justify="center">
+        {
+          <Typography inline className={classes.annoucement}>
+              <span>Our V1 Vaults are available at <a href="https://v1.robo-vault.com/">v1.robo-vault.com</a></span><br/>
+              <span>For any questions on migration, please see <a href="https://docs.robo-vault.com/robovault-v2/robovault-v2-faq">V2 FAQ</a></span>
+          </Typography>
+        }
+      </Grid>
+      <Grid container item className={classes.root} justify="center">
+        {
+          <Typography className={classes.disclaimer}>{t('Disclaimer')} 
+              <a href="https://docs.robo-vault.com/robovault/risks" rel="noreferrer">
+                <i className={`fas fa-book ${classes.linkIcon}`}></i>
+              </a>
+          </Typography>
+        }
+      </Grid>
+    </>
   );
 };
 
