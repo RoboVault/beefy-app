9639dcd338398df0a6d505e2f4030bb02d217a24
SmoothBot <SmoothBot>
2021-10-06 20:30:47 +1100

    Updating risks url

--- src/components/Disclaimer/Disclaimer.js
+++ src/components/Disclaimer/Disclaimer.js
@@ -25,7 +25,7 @@ const Disclaimer = () => {
       <Grid container item className={classes.root} justify="center">
         {
           <Typography className={classes.disclaimer}>{t('Disclaimer')} 
-              <a href="https://docs.robo-vault.com/robovault/risks" rel="noreferrer">
+              <a href="https://docs.robo-vault.com/security/risks" rel="noreferrer">
                 <i className={`fas fa-book ${classes.linkIcon}`}></i>
               </a>
           </Typography>
