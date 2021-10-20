1c0de1c04998bfe3512be72d32cb26c772d0354d
SmoothBot <SmoothBot>
2021-10-20 22:25:35 +1100

    Remove v1 link

--- src/components/Disclaimer/Disclaimer.js
+++ src/components/Disclaimer/Disclaimer.js
@@ -16,14 +16,6 @@ const Disclaimer = () => {
     <>
       <Grid container item className={classes.root} justify="center">
         {
-          <Typography inline className={classes.annoucement}>
-              <span>Our V1 Vaults are available at <a href="https://v1.robo-vault.com/">v1.robo-vault.com</a></span><br/>
-              <span>For any questions on migration, please see <a href="https://docs.robo-vault.com/robovault-v2/robovault-v2-faq">V2 FAQ</a></span>
-          </Typography>
-        }
-      </Grid>
-      <Grid container item className={classes.root} justify="center">
-        {
           <Typography className={classes.disclaimer}>{t('Disclaimer')} 
               <a href="https://docs.robo-vault.com/security/risks" rel="noreferrer">
                 <i className={`fas fa-book ${classes.linkIcon}`}></i>
