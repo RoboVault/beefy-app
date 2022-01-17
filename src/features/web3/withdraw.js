db7bdf52c36b685492f81635818205ddb1157891
SmoothBot <smoothbot123@gmail.com>
2022-01-17 16:22:15 +1100

    Adding USDC + 0.2% slippage

--- src/features/web3/withdraw.js
+++ src/features/web3/withdraw.js
@@ -9,9 +9,8 @@ export const withdraw = async ({ web3, address, isAll, amount, contractAddress, 
 
 const _withdraw = ({ web3, contract, address, isAll, amount, dispatch }) => {
   return new Promise((resolve, reject) => {
-    if (isAll) {
       contract.methods
-        .withdraw()
+        .withdraw(amount, address, 20)
         .send({ from: address })
         .on('transactionHash', function (hash) {
           console.log(hash);
@@ -38,35 +37,6 @@ const _withdraw = ({ web3, contract, address, isAll, amount, dispatch }) => {
           console.log(error);
           reject(error);
         });
-    } else {
-      contract.methods
-        .withdraw(amount)
-        .send({ from: address })
-        .on('transactionHash', function (hash) {
-          console.log(hash);
-          dispatch(
-            enqueueSnackbar({
-              message: hash,
-              options: {
-                key: new Date().getTime() + Math.random(),
-                variant: 'success',
-              },
-              hash,
-            })
-          );
-        })
-        .on('receipt', function (receipt) {
-          console.log(receipt);
-          resolve();
-        })
-        .on('error', function (error) {
-          console.log(error);
-          reject(error);
-        })
-        .catch(error => {
-          console.log(error);
-          reject(error);
-        });
-    }
+    // }
   });
 };
